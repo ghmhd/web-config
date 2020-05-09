e709ed1b7aa84296cbe19741cc6a58313466288d
Gharbi Mohamed <ghmhdweb1996@gmail.com>
2020-05-09 06:34:49 +0100

    init

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+      }
+    },
+  },
+  purge: [
+    './src/**/*.svelte',
+    './src/**/*.html',
+    './static/**/*.html',
+    './resources/views/**/*.edge'
+  ],
+  variants: {},
+  plugins: [],
+}
